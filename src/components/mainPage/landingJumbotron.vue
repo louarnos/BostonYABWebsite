a57c93d7b2c255bb007d509c1d447763d2032d40
louarnos <louarnos@gmail.com>
2019-02-18 16:05:16 -0500

    Update jumbo for mobile

--- src/components/mainPage/landingJumbotron.vue
+++ src/components/mainPage/landingJumbotron.vue
@@ -1,29 +1,36 @@
 <template>
-    <v-parallax :src="require('@/assets/downtown.jpg')">
-       <v-layout row class="space"/>
-       <v-layout fill-height align-start >
-         <v-flex xs6>
-          <span class="white--text landing-text landing-text">Nothing About Us, Without Us.</span>
+    <v-img :height="!isMobile ? 800 : 400" :src="!isMobile ? require('@/assets/yabPainting.jpg') : require('@/assets/yabPaintingMobile.jpg')" class="offset-for-nav">
+       <v-layout fill-height align-end >
+       <v-flex xs12>
+          <span class="yellow--text landing-text landing-text">Nothing About Us, Without Us.</span>
          </v-flex>
        </v-layout>
-     </v-parallax>
+     </v-img>
 </template>
 <script>
-import "vuetify/dist/vuetify.min.js"
-import "vuetify/dist/vuetify.min.css"
+import Mobile from "../common/mobile.vue"
 export default {
   name: 'LandingJumbo',
+  mixins: [Mobile],
   created() {
+      console.log( this.isMobile );
       console.log('jumbo created');
   },
 }
 </script>
 <style scoped>
 .landing-text {
-  font-size: 3em;
+  font-size: 4em;
   font-weight: 100;
 }
-.space {
-  height: 2000px;
+.offset-for-nav {
+    margin-top: 63px;
 }
+@media screen and (max-width: 480px) {
+  .landing-text {
+    font-size: 1.5em;
+    font-weight: 500;
+  }
+}
+
 </style>
