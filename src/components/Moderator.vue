b32a112a76e6fb629cf6423f6212b5cfd4df54fb
louarnos <louarnos@gmail.com>
2019-02-18 13:32:37 -0500

    Add hasUser mixin to handle for user info/authentication

--- src/components/Moderator.vue
+++ src/components/Moderator.vue
@@ -1,119 +1,72 @@
 <template>
-    <div class="indigo lighten-2 white--text text-xs-center login-container" >
-        <div class="section-header">
-          <h1> Create New Post </h1>
-        </div>
-		<v-form
-		ref="form"
-		lazy-validation>
-			<v-container>
-				<v-layout row wrap>
-					<v-flex xs6 offset-sm3>
-						<v-text-field
-						  v-model="title"
-                          class="form-field-container"
-						  label="Title"
-                          solo>
-						</v-text-field>
-					</v-flex>
-					<v-flex xs6 offset-sm3>
-                        <div class="form-field-container">
-                            <multi-select
-                                id="author"
-                                v-model="author"
-                                :taggable="true"
-                                @tag="addTag"
-                                :options="authorOptions"
-                                placeholder="Author">
-                            </multi-select>
-                        </div>
-					</v-flex>
-					<v-flex xs6 offset-sm3>
-                        <div class="form-field-container">
-                            <multi-select
-                                id="tags"
-                                v-model="tags"
-                                :taggable="true"
-                                @tag="addTag"
-                                multiple="multiple"
-                                :options="tagsOptions"
-                                placeholder="Tags">
-                            </multi-select>
-                        </div>
-					</v-flex>
-					<v-flex xs6 offset-sm3>
-						<v-text-field
-							textarea
-							rows="15"
-							auto-grow
-							counter=4000
-							v-model="body"
-							class="form-field-container"
-							label="Body"
-							solo
-							auto-grow>
-						</v-text-field>
-					</v-flex>
-					<v-flex xs6 offset-sm3>
-                        <v-btn
-                            :color="'info'"
-                            @click="createPost" >
-                            Create
-                        </v-btn>
-					</v-flex>
-				</v-layout>
-			</v-container>
-        </v-form>
-   </div>
+<div>
+   <v-navigation-drawer
+     floating
+     permanent
+     stateless
+	 app
+
+     v-model="drawer">
+     <v-toolbar flat class="transparent">
+       <v-list class="pa-0">
+         <v-list-tile >
+           <v-list-tile-content>
+             <v-list-tile-title> Welcome Back {{ yabName.split(' ').map( w => w.charAt(0).toUpperCase() + w.slice(1) ).join(' ')  }}! </v-list-tile-title>
+           </v-list-tile-content>
+         </v-list-tile>
+       </v-list>
+     </v-toolbar>
+
+     <v-list class="pt-0" dense>
+       <v-divider></v-divider>
+
+       <v-list-tile
+         v-for="item in navItems"
+         :key="item.title"
+         @click="updateRoute(item.path)">
+         <v-list-tile-action>
+           <v-icon>{{ item.icon }}</v-icon>
+         </v-list-tile-action>
+
+         <v-list-tile-content>
+           <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+         </v-list-tile-content>
+       </v-list-tile>
+     </v-list>
+   </v-navigation-drawer>
+   <v-content>
+	 <router-view></router-view>
+   </v-content>
+</div>
 </template>
 <script>
 
-import axios from 'axios';
 import Multiselect from 'vue-multiselect'
+import User from './common/hasUser.vue'
 
 export default {
   name: 'Moderator',
   components: {
       'multi-select': Multiselect,
   },
+  mixins: [User],
   methods: {
-      addTag(newTag, field) {
-          this[`${field}Options`].push(newTag);
-          this[field].push(newTag);
-      },
-      createPost() {
-          console.log('create post');
-      }
+	  updateRoute(path) {
+		  this.$router.push(path);
+	  },
   },
   data: () => ({
-      author: [],
-      title: "",
-      body: "",
-      tags: [],
-      tagsOptions: [],
-      authorOptions: [],
+	navItems: [
+	  { title: 'Add Author', icon: 'how_to_reg', path: '/moderator/add_author'  },
+	  { title: 'Authors', icon: 'people', path: '/moderator/authors'  },
+      { title: 'Create Post', icon: 'create' , path: '/moderator/create_post' },
+	  { title: 'Posts', icon: 'list', path: '/moderator/posts'  },
+    ],
+	drawer: true,
+	right: null,
+	view: 'createPost',
   }),
-  created() {
-      console.log( 'created' );
-      console.log( localStorage.getItem('token') );
-  }
 }
 </script>
 <style scoped>
-  .login-container {
-      border: 3px solid #971111;
-      background-color: #DDD;
-      padding-top: 85px;
-  }
-  .form-field-container {
-      color: black;
-      margin-top: 1%;
-      margin-bottom: 1%;
-      background-color: white;
-  }
-  .section-header{
-      font-size: 1.5em;
-      font-weight: 300;
-      font-family: Monsterrat, sans-serif;
-  }
 </style>
---
+++ src/components/common/hasUser.vue
@@ -0,0 +1,16 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'hasUser',
+  computed: {
+      yabName() { return localStorage.getItem('yabUsersName') },
+      yabUsername() { return localStorage.getItem('yabUsername') },
+      yabEmail(){ return localStorage.getItem('yabUserEmail') },
+      yabToken(){ return localStorage.getItem('yabToken') },
+  },
+  created() {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${ this.yabToken }`;
+  },
+}
+</script>
