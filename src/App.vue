43d6a017591b3465d0d9c197855d297f89d54bcd
louarnos <louarnos@gmail.com>
2019-02-21 20:07:06 -0500

    Update global style

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,21 @@ export default {
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style>
+  .v-content {
+      height: 100%;
+  }
+  #app {
+      height: 100%;
+  }
+  .layout.column {
+      height: 100%;
+  }
+  html {
+      height: 100%;
+  }
   body {
       font-family: Monsterrat, sans-serif;
+      height: 100%;
   }
   .theme--light.v-label {
       color: #FFA000;
@@ -22,8 +35,12 @@ export default {
   }
   .v-input__slot {
       background-color: #FAFAFA;
-      margin: .5%;
       padding-left: 1%;
+      height: 40px;
+      border-radius: 5px;
+  }
+  .theme--light.v-text-field > .v-input__control > .v-input__slot::before {
+      border: none;
   }
   .v-messages__message {
       color: #E53935;
