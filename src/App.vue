8df873fd32dad166cdfca624e194099f4a653060
louarnos <louarnos@gmail.com>
2019-02-23 13:36:33 -0500

    Update global styles

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,14 @@ export default {
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style>
-  .v-content {
-      height: 100%;
-  }
-  #app {
-      height: 100%;
-  }
-  .layout.column {
-      height: 100%;
-  }
   html {
       height: 100%;
+      margin: 0;
   }
   body {
       font-family: Monsterrat, sans-serif;
       height: 100%;
+      margin: 0;
   }
   .theme--light.v-label {
       color: #FFA000;
@@ -36,11 +29,11 @@ export default {
   .v-input__slot {
       background-color: #FAFAFA;
       padding-left: 1%;
-      height: 40px;
       border-radius: 5px;
   }
   .theme--light.v-text-field > .v-input__control > .v-input__slot::before {
       border: none;
+      border-color: #FFA000;
   }
   .v-messages__message {
       color: #E53935;
@@ -48,5 +41,22 @@ export default {
   }
   .v-btn.theme--light {
       color: #FFA000;
+  }
+  .ck-editor__main > .ck.ck-content.ck-editor__editable {
+      min-height: 300px !important;
+      color: black;
+      text-align: left;
+  }
+  .multiselect, .multiselect__select, .multiselect__tags {
+      max-height: 32px !important;
+      min-height: 32px !important;
+  }
+  .multiselect__tags {
+      padding: 4px;
+      padding-left: 2px;
+  }
+  .multiselect__placeholder {
+      color: #FFA000;
+      font-size: 16px;
   }
 </style>
