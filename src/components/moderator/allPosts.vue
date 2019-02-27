af2776036e77efa048a67f7f685a93479cc0c708
louarnos <louarnos@gmail.com>
2019-02-26 22:34:23 -0500

    Build out all posts display
    Need edit interface

--- src/components/moderator/allPosts.vue
+++ src/components/moderator/allPosts.vue
@@ -1,11 +1,42 @@
 <template>
-    <div> All posts </div>
+  <div class="indigo lighten-2 white--text text-xs-center login-container" >
+    <div class="section-header">
+      <h1> Posts </h1>
+    </div>
+    <v-container fluid>
+      <v-layout row wrap>
+        <v-flex xs12 v-for="( post, i ) in posts" :key="i" class="bottom-margin">
+            <blog-post :title="post.title" :author="post.author" :body="post.body" :tags="post.tags" :files="post.files" :forDisplay="true"/>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import BlogPost from '../common/blogPost.vue'
 export default {
-  name: 'AllPosts'
+  name: 'AllPosts',
+  props: ['posts'],
+  components: {
+      'blog-post': BlogPost,
+  }
 }
 </script>
-<style>
+<style scoped>
+  .login-container {
+      border: 3px solid #971111;
+      background-color: #DDD;
+      padding-top: 85px;
+      width: 100%;
+      height: 100%;
+  }
+  .section-header > h1 {
+      font-size: 3em;
+      font-weight: 300;
+      font-family: Monsterrat, sans-serif;
+  }
+  .bottom-margin {
+      margin-bottom: 3%;
+  }
 </style>
